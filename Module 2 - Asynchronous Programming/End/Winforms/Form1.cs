a8d4fe0dc5bb12482f19be1b3c6d914691919b16
Felipe Gavilan <elmejor_887@hotmail.com>
2020-09-01 19:38:20 -0400

    Added Module 2

--- Module 2 - Asynchronous Programming/End/Winforms/Form1.cs
+++ Module 2 - Asynchronous Programming/End/Winforms/Form1.cs
@@ -31,70 +31,70 @@ namespace Winforms
             loadingGIF.Visible = true;
 
             // Video: Non-Freezing UI
-            new UI_Not_Freezes().SyncVersion();
-            await new UI_Not_Freezes().AsyncVersion();
+            //new UI_Not_Freezes().SyncVersion();
+            //await new UI_Not_Freezes().AsyncVersion();
 
-            // Videos: Task and Task That Returns a Value
-            await new Task_And_TaskOfT(apiURL).btnStart_Click(txtInput);
+            // Video: Task and Task That Returns a Value
+            //await new Task_And_TaskOfT(apiURL).btnStart_Click(txtInput);
 
-            // Videos: Task with Errors
-            await new Task_With_Errors(apiURL).btnStart_Click(txtInput);
+            // Video: Task with Errors
+            //await new Task_With_Errors(apiURL).btnStart_Click(txtInput);
 
-            // Videos: Executing Multiple Tasks - Task.WhenAll
-            await new Task_WhenAll(apiURL).btnStart_Click();
+            // Video: Executing Multiple Tasks - Task.WhenAll
+            //await new Task_WhenAll(apiURL).btnStart_Click();
 
-            // Videos: Offloading the Current Thread - Task.Run
-            await new Task_Run(apiURL).btnStart_Click();
+            // Video: Offloading the Current Thread - Task.Run
+            //await new Task_Run(apiURL).btnStart_Click();
 
-            // Videos: Limiting the Amount of Concurrent Tasks - SemaphoreSlim
-            await new SemaphoreExample(apiURL).btnStart_Click();
+            // Video: Limiting the Amount of Concurrent Tasks - SemaphoreSlim
+            //await new SemaphoreExample(apiURL).btnStart_Click();
 
-            // Videos: Using the Response Task.WhenAll
-            await new Response_Of_Task_WhenAll(apiURL).btnStart_Click();
+            // Video: Using the Response Task.WhenAll
+            //await new Response_Of_Task_WhenAll(apiURL).btnStart_Click();
 
-            // Videos: Reporting Progress with IProgress
-            await new Reporting_Progress(pgCards, apiURL).btnStart_Click();
+            // Video: Reporting Progress with IProgress
+            //await new Reporting_Progress(pgCards, apiURL).btnStart_Click();
 
-            // Videos: Reporting Progress in Intervals - Task.WhenAny
-            await new Reporting_Progress_Intervals(pgCards, apiURL).btnStart_Click();
+            // Video: Reporting Progress in Intervals - Task.WhenAny
+            //await new Reporting_Progress_Intervals(pgCards, apiURL).btnStart_Click();
 
-            // Videos: Cancelling Tasks
-            cancellationTokenSource = new CancellationTokenSource();
-            await new Cancelling_Tasks(pgCards, apiURL, cancellationTokenSource).btnStart_Click();
+            // Video: Cancelling Tasks
+            //cancellationTokenSource = new CancellationTokenSource();
+            //await new Cancelling_Tasks(pgCards, apiURL, cancellationTokenSource).btnStart_Click();
 
-            // Videos: Cancelling Loops
-            cancellationTokenSource = new CancellationTokenSource();
-            await new Cancelling_Loops(pgCards, apiURL, cancellationTokenSource).btnStart_Click();
+            // Video: Cancelling Loops
+            //cancellationTokenSource = new CancellationTokenSource();
+            //await new Cancelling_Loops(pgCards, apiURL, cancellationTokenSource).btnStart_Click();
 
-            // Videos: Cancelling with a Timeout
-            cancellationTokenSource = new CancellationTokenSource();
-            cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
-            await new Cancelling_Timeout(pgCards, apiURL, cancellationTokenSource).btnStart_Click();
+            // Video: Cancelling with a Timeout
+            //cancellationTokenSource = new CancellationTokenSource();
+            //cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+            //await new Cancelling_Timeout(pgCards, apiURL, cancellationTokenSource).btnStart_Click();
 
-            // Videos: Creating Finished Tasks - Task.FromResult and Friends
-            var creating_Finished_Tasks = new Creating_Finished_Tasks();
-            var cards = await creating_Finished_Tasks.GetCardsMock(1);
-            await creating_Finished_Tasks.ProcessCardsMock(cards);
-            await creating_Finished_Tasks.GetCancelledTask();
-            await creating_Finished_Tasks.GetTaskWithError();  // This throws an error is awaited
+            // Video: Creating Finished Tasks - Task.FromResult and Friends
+            //var creating_Finished_Tasks = new Creating_Finished_Tasks();
+            //var cards = await creating_Finished_Tasks.GetCardsMock(1);
+            //await creating_Finished_Tasks.ProcessCardsMock(cards);
+            //await creating_Finished_Tasks.GetCancelledTask();
+            //await creating_Finished_Tasks.GetTaskWithError();  // This throws an error is awaited
 
-            // Videos: Ignoring the SynchronizationContext - ConfigureAwait
-            CheckForIllegalCrossThreadCalls = true; // Only necessary in WinForms apps
-            await new ConfigureAwait().btnStart_Click(btnCancelar);
+            // Video: Ignoring the SynchronizationContext - ConfigureAwait
+            //CheckForIllegalCrossThreadCalls = true; // Only necessary in WinForms apps
+            //await new ConfigureAwait().btnStart_Click(btnCancelar);
 
-            // Videos: Retry Pattern
-            await new RetryPattern(apiURL).btnStart_Click();
+            // Video: Retry Pattern
+            //await new RetryPattern(apiURL).btnStart_Click();
 
-            // Videos: Only-One Pattern
-            cancellationTokenSource = new CancellationTokenSource();
-            await new Only_One_Pattern(apiURL, cancellationTokenSource).btnIniciar_Click();
+            // Video: Only-One Pattern
+            //cancellationTokenSource = new CancellationTokenSource();
+            //await new Only_One_Pattern(apiURL, cancellationTokenSource).btnIniciar_Click();
 
-            // Videos: Controlling the Result of a Task - TaskCompletionSource
-            await new TaskCompletionSource_Example().btnStart_Click(txtInput);
+            // Video: Controlling the Result of a Task - TaskCompletionSource
+            //await new TaskCompletionSource_Example().btnStart_Click(txtInput);
 
-            // Videos: Cancelling Non-Cancellable Tasks with TaskCompletionSource
-            cancellationTokenSource = new CancellationTokenSource();
-            await new Cancelling_Non_Cancellable_Tasks(cancellationTokenSource).btnStart_Click();
+            // Video: Cancelling Non-Cancellable Tasks with TaskCompletionSource
+            //cancellationTokenSource = new CancellationTokenSource();
+            //await new Cancelling_Non_Cancellable_Tasks(cancellationTokenSource).btnStart_Click();
 
             loadingGIF.Visible = false;
         }
